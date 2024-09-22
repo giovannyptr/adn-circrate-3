5094238af3c2b5e590a1150bc0f4ee44ac0b1eb8
Giovanny Putri Andini <86881191+giovannyptr@users.noreply.github.com>
2024-09-22 23:09:07 +0700

    fix more bugs

--- src/app/_components/sections/Discover.jsx
+++ src/app/_components/sections/Discover.jsx
@@ -1,13 +1,8 @@
-"use client"; // Add this at the top of your file
-
-import React from "react"; // Import React if not already done
+import React from "react";
+import Link from 'next/link';
 import Data from "@data/sections/discover.json";
 
 const DiscoverSection = () => {
-  const handleEmailClick = () => {
-    window.location.href = "mailto:circrete@gmail.com";
-  };
-
   return (
     <section style={{ backgroundColor: "#F1F5EB", paddingTop: "60px" }}>
       <div className="container mil-p-0-90">
@@ -16,30 +11,22 @@ const DiscoverSection = () => {
         <div className="row justify-content-between">
           <div className="col-lg-6">
             <div>
-              <h2
-                className="mil-upper mil-up mil-mb-40"
-                dangerouslySetInnerHTML={{ __html: Data.title }}
-              />
+              <h2 className="mil-upper mil-up mil-mb-40" dangerouslySetInnerHTML={{ __html: Data.title }} />
             </div>
           </div>
 
           <div className="col-lg-6">
-            <p
-              className="mil-up mil-mb-60"
-              dangerouslySetInnerHTML={{ __html: Data.description }}
-            />
+            <p className="mil-up mil-mb-60" dangerouslySetInnerHTML={{ __html: Data.description }} />
 
             <div className="row">
-              <button
-                onClick={handleEmailClick}
-                className="mil-link mil-upper mil-up"
-                style={{ background: 'none', border: 'none', cursor: 'pointer', padding: 0 }}
-              >
-                {Data.button.label}
-                <span className="mil-arrow">
-                  <img src="img/icons/1.svg" alt="arrow" />
-                </span>
-              </button>
+              <Link href={Data.button.link} legacyBehavior>
+                <a className="mil-link mil-upper mil-up" style={{ cursor: 'pointer' }}>
+                  {Data.button.label}
+                  <span className="mil-arrow">
+                    <img src="img/icons/1.svg" alt="arrow" />
+                  </span>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
